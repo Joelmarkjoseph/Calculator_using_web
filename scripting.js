842c5b83b47dabd8bea28e82d8b09f8a72870bc7
Joel Mark Joseph S <joelmarkjoseph2004@gmail.com>
2023-10-24 00:23:49 +0530

    New Commits

--- scripting.js
+++ scripting.js
@@ -1,90 +1,128 @@
 var arr=[];
 var ans=0;
+var inpnum=0;
+//Numbers
+
 function fun1(){
-    document.getElementById('Input_field').textContent+='1 ';
-    arr.push(1);
+    document.getElementById('Input_field').textContent+='1';
+    take(1);
 }
 function fun2(){
-    document.getElementById('Input_field').textContent+='2 ';
-    arr.push(2);
+    document.getElementById('Input_field').textContent+='2';
+    take(2);
 }
 function fun3(){
-    document.getElementById('Input_field').textContent+='3 ';
-    arr.push(3);
+    document.getElementById('Input_field').textContent+='3';
+    take(3);
 }
 function fun4(){
-    document.getElementById('Input_field').textContent+='4 ';
-    arr.push(4);
+    document.getElementById('Input_field').textContent+='4';
+    take(4);
 }
 function fun5(){
-    document.getElementById('Input_field').textContent+='5 ';
-    arr.push(5);
+    document.getElementById('Input_field').textContent+='5';
+    take(5);
 }
 function fun6(){
-    document.getElementById('Input_field').textContent+='6 ';
-    arr.push(6);
+    document.getElementById('Input_field').textContent+='6';
+    take(6);
 }
 function fun7(){
-    document.getElementById('Input_field').textContent+='7 ';
-    arr.push(7);
+    document.getElementById('Input_field').textContent+='7';
+    take(7);
 }
 function fun8(){
-    document.getElementById('Input_field').textContent+='8 ';
-    arr.push(8);
+    document.getElementById('Input_field').textContent+='8';
+    take(8);
 }
 function fun9(){
-    document.getElementById('Input_field').textContent+='9 ';
-    arr.push(9);
+    document.getElementById('Input_field').textContent+='9';
+    take(9);
 }
 function fun0(){
-    document.getElementById('Input_field').textContent+='0 ';
-    arr.push(0);
+    document.getElementById('Input_field').textContent+='0';
+    take(0);
 }
+
+//operators
+
 function funplus(){
-    document.getElementById('Input_field').textContent+='+ ';
+    document.getElementById('Input_field').textContent+='+';
+    arr.push(inpnum);
+    inpnum=0;
     arr.push("+");
 }
 function funmin(){
-    document.getElementById('Input_field').textContent+='- ';
+    document.getElementById('Input_field').textContent+='-';
+    arr.push(inpnum);
+    inpnum=0;
     arr.push("-");
 }
 function fundiv(){
-    document.getElementById('Input_field').textContent+='/ ';
+    document.getElementById('Input_field').textContent+='/';
+    arr.push(inpnum);
+    inpnum=0;
     arr.push("/");
 }
 function funx(){
-    document.getElementById('Input_field').textContent+='x ';
+    document.getElementById('Input_field').textContent+='x';
+    arr.push(inpnum);
+    inpnum=0;
     arr.push("x");
 }
+function funper(){
+    document.getElementById('Input_field').textContent+='%';
+    arr.push(inpnum);
+    inpnum=0;
+    arr.push("%");
+}
 function funac(){
-    document.getElementById('Input_field').textContent=' ';
+    document.getElementById('Input_field').textContent='0';
     ans=0;
+    inpnum=0;
     arr=[];
 }
+function funcl(){
+    inpnum=Math.floor(inpnum/10);
+    document.getElementById('Input_field').textContent=inpnum;
+}
+
 function funeq(){
+    arr.push(inpnum);
+    inpnum=0;
     ans=arr[0];
     if(arr[1]==="+"){
         ans+=arr[2];
         arr=[];
-        arr.push(ans);
+        inpnum=ans;
     }
     if(arr[1]==="-"){
         ans-=arr[2];
         arr=[];
-        arr.push(ans);
+        inpnum=ans;
 
     }
     if(arr[1]==="/"){
         ans/=arr[2];
         arr=[];
-        arr.push(ans);
+        inpnum=ans;
 
     }
     if(arr[1]==="x"){
         ans*=arr[2];
         arr=[];
-        arr.push(ans);
+        inpnum=ans;
+
+    }
+    if(arr[1]==="%"){
+        ans%=arr[2];
+        arr=[];
+        inpnum=ans;
 
     }
     document.getElementById('Input_field').textContent=ans;
+}
+
+function take(n){
+    inpnum=inpnum*10+n;
 }
